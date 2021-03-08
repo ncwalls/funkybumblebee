1ecb7abc78c14f4ad4fa345844c78f020c83414a
Nathan <ncwalls@gmail.com>
2021-03-07 20:35:25 -0500

    stupid webpage

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en-US" class="no-js no-svg">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<title>Funky Bumblebee</title>
+	<link rel="canonical" href="https://www.funkybumblebee.com/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Funky Bumblebee" />
+	<meta property="og:url" content="https://www.funkybumblebee.com/" />
+	<meta property="og:site_name" content="Funky Bumblebee" />
+	<link rel="icon" type="image/png" href="favicon.png">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div class="container">
+		<img src="bumbl-optimiz.png" alt="Funky Bumblebee" class="bee">
+	</div>
+</body>
+</html>
